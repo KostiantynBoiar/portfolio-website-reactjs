6555ca85ff12faf60710c0e360c687dfd9712497
Kostiantyn Boiar <opezdalya@gmail.com>
2023-09-21 07:20:07 +0100

    Home page has done

--- react-protfolio-project/src/components/Home/index.js
+++ react-protfolio-project/src/components/Home/index.js
@@ -1,3 +1,19 @@
+import { Link } from "react-router-dom";
+import './index.scss'
+
 const Home = () => {
-    
+    return(
+        <>
+            <div className="container home-page">
+                <div className="text-zone">
+                    <h1>Hi,<br/>I'm Kostiantyn</h1>
+                    
+                    <h2>Fullstack Developer / Software Engineer</h2>
+                    <Link to="/contact" className="flat-button">CONTACT ME</Link>
+                </div>
+            </div>
+        </>
+    );
 }
+
+export default Home
